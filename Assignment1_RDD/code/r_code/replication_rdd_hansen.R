2017e486f44eb9f9cd7d760d317f48b719b04978
Nathan <37715299+nfra@users.noreply.github.com>
2020-02-25 12:41:40 -0600

    Created files for replication code and Rmd

---
+++ Assignment1_RDD/code/r_code/replication_rdd_hansen.R
@@ -0,0 +1,10 @@
+# Name: replication_rdd_hansen
+# Written by: Nathan Franz
+# Description: A replication of several main figures and tables from the 2015 
+# 			   article "Punishment and Deterrence: Evidence from Drunk Driving"
+# 			   by Benjamin Hansen
+# Last edited: 2/25/2020
+
+
+
+
---
+++ Assignment1_RDD/code/stata_code/replication_rdd_hansen.do
@@ -0,0 +1,7 @@
+* Name: replication_rdd_hansen
+* Written by: Nathan Franz
+* Description: A replication of several main figures and tables from the 2015 
+* 			   article "Punishment and Deterrence: Evidence from Drunk Driving"
+* 			   by Benjamin Hansen
+* Last edited: 2/25/2020
+
