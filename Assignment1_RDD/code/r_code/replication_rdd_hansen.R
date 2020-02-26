6a4cf4f3a9d94fdc4567a2d8008737b33e3e08de
Nathan <37715299+nfra@users.noreply.github.com>
2020-02-26 16:38:57 -0600

    completed figure 2 replication
    weird discrepancy between Panel B in paper and my replication

--- Assignment1_RDD/code/r_code/replication_rdd_hansen.R
+++ Assignment1_RDD/code/r_code/replication_rdd_hansen.R
@@ -7,6 +7,7 @@
 
 library(tidyverse)
 library(ggthemes)
+library(gridExtra)
 
 setwd("C:/Users/Nathan/Documents/UT Courses/3. Spring 2019/Causal Inference/CausalInferenceCourseHomework/Assignment1_RDD") 
 
@@ -19,7 +20,7 @@ dwi = read.csv("./data/hansen_dwi.csv")
 #   bac_overlimit is an indicator variable for whether BAC is over the 0.08% limit
 
 dwi$bac_min <- apply(dwi[,c('bac1', 'bac2')], 1, min)
-dwi$over_limit <- as.numeric(eval(dwi$bac_min >= 0.08))
+dwi$over_limit <- as.numeric(eval(dwi$bac_min >= 0.08)) + as.numeric(eval(dwi$bac_min >= 0.15)) 
 
 # recreate figure 1, histogram of bac_min
 
@@ -36,10 +37,106 @@ manipulation_histogram
 
 # check for covariate balance, estimating equation 1
 
-lm_recid <- lm(recidivism ~ male + white + aged + 
+lm_recid <- lm(recidivism ~ male + white + aged + acc +
                 over_limit + bac_min + over_limit*bac_min, 
               data=dwi)
 summary(lm_recid)
 
+# recreate figure 2, panels A-D
 
+dwi$bac_bucket <- cut(dwi$bac_min, seq(from=0, to=0.436, by=0.002), 
+                      labels=(seq(from=0.001, to=0.435, by=0.002)),
+                      include.lowest=TRUE)
 
+dwi_accident_sum <- dwi %>%
+  group_by(bac_bucket) %>%
+  summarize(
+    mean_male = mean(male),
+    mean_white = mean(white),
+    mean_acc = mean(acc),
+    mean_aged = mean(aged),
+    number_obs = n(),
+    over_limit = unique(as.numeric(eval(bac_min >= 0.08)))
+  )
+
+dwi_accident_sum$bac_min <- as.numeric(as.character(dwi_accident_sum$bac_bucket))
+
+# panel A - accident at scene
+
+dwi_accident_sum$acc <- dwi_accident_sum$mean_acc
+
+panel_a <- ggplot(data = dwi, 
+       aes(x = bac_min, 
+           y = acc, 
+           group = over_limit)) +
+  theme_clean() +
+  geom_point(data = dwi_accident_sum) +
+  geom_smooth(method='lm') +
+  geom_vline(xintercept = 0.08) +
+  geom_vline(xintercept = 0.15) +
+  labs(title="Panel A. Accident at scene") +
+  xlab("BAC") +
+  ylab("") +
+  coord_cartesian(xlim = c(0.03, 0.2), ylim = c(0.05, 0.25))
+panel_a
+
+# panel B - male
+  
+dwi_accident_sum$male <- dwi_accident_sum$mean_male
+
+panel_b <- ggplot(data = dwi, 
+                  aes(x = bac_min, 
+                      y = male, 
+                      group = over_limit)) +
+  theme_clean() +
+  geom_point(data = dwi_accident_sum) +
+  geom_smooth(method='lm') +
+  geom_vline(xintercept = 0.08) +
+  geom_vline(xintercept = 0.15) +
+  labs(title="Panel B. Male") +
+  xlab("BAC") +
+  ylab("") +
+  coord_cartesian(xlim = c(0.03, 0.2), ylim = c(0.74, 0.83))
+panel_b
+
+# panel C - age
+
+dwi_accident_sum$aged <- dwi_accident_sum$mean_aged
+
+panel_c <- ggplot(data = dwi, 
+                  aes(x = bac_min, 
+                      y = aged, 
+                      group = over_limit)) +
+  theme_clean() +
+  geom_point(data = dwi_accident_sum) +
+  geom_smooth(method='lm') +
+  geom_vline(xintercept = 0.08) +
+  geom_vline(xintercept = 0.15) +
+  labs(title="Panel C. Age") +
+  xlab("BAC") +
+  ylab("") +
+  coord_cartesian(xlim = c(0.03, 0.2), ylim = c(0.05, 0.25))
+panel_c
+
+# panel D - white
+
+dwi_accident_sum$white <- dwi_accident_sum$mean_white
+
+panel_d <- ggplot(data = dwi, 
+                  aes(x = bac_min, 
+                      y = white, 
+                      group = over_limit)) +
+  theme_clean() +
+  geom_point(data = dwi_accident_sum) +
+  geom_smooth(method='lm') +
+  geom_vline(xintercept = 0.08) +
+  geom_vline(xintercept = 0.15) +
+  labs(title="Panel D. White") +
+  xlab("BAC") +
+  ylab("") +
+  coord_cartesian(xlim=c(0.03, 0.2), ylim=c(0.05, 0.25))
+panel_d
+
+#
+figure2_rep <- grid.arrange(panel_a, panel_b, panel_c, panel_d, nrow = 2)
+figure2_rep
