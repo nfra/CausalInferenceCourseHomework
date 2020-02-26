4503991b176fa85138a5ea23b204fe9705e68008
Nathan <37715299+nfra@users.noreply.github.com>
2020-02-26 17:58:04 -0600

    beautified graphs

--- Assignment1_RDD/code/r_code/replication_rdd_hansen.R
+++ Assignment1_RDD/code/r_code/replication_rdd_hansen.R
@@ -115,7 +115,7 @@ panel_c <- ggplot(data = dwi,
   labs(title="Panel C. Age") +
   xlab("BAC") +
   ylab("") +
-  coord_cartesian(xlim = c(0.03, 0.2), ylim = c(0.05, 0.25))
+  coord_cartesian(xlim = c(0.03, 0.2), ylim = c(33, 38))
 panel_c
 
 # panel D - white
@@ -134,9 +134,87 @@ panel_d <- ggplot(data = dwi,
   labs(title="Panel D. White") +
   xlab("BAC") +
   ylab("") +
-  coord_cartesian(xlim=c(0.03, 0.2), ylim=c(0.05, 0.25))
+  coord_cartesian(xlim=c(0.03, 0.2), ylim=c(0.8, 0.9))
 panel_d
 
-#
+# Assemble panels into figure 2 replication
 figure2_rep <- grid.arrange(panel_a, panel_b, panel_c, panel_d, nrow = 2)
 figure2_rep
+
+
+
+
+# recreate figure 2, panels A-D, but with quadratic model
+
+# panel A - accident at scene
+
+panel_a_quadratic <- ggplot(data = dwi, 
+                  aes(x = bac_min, 
+                      y = acc, 
+                      group = over_limit)) +
+  theme_clean() +
+  geom_point(data = dwi_accident_sum) +
+  geom_smooth(method = 'lm', formula= y ~ poly(x,2)) +
+  geom_vline(xintercept = 0.08) +
+  geom_vline(xintercept = 0.15) +
+  labs(title="Panel A. Accident at scene") +
+  xlab("BAC") +
+  ylab("") +
+  coord_cartesian(xlim = c(0.03, 0.2), ylim = c(0.05, 0.25))
+panel_a_quadratic
+
+# panel B - male
+
+panel_b_quadratic <- ggplot(data = dwi, 
+                  aes(x = bac_min, 
+                      y = male, 
+                      group = over_limit)) +
+  theme_clean() +
+  geom_point(data = dwi_accident_sum) +
+  geom_smooth(method = 'lm', formula= y ~ poly(x,2)) +
+  geom_vline(xintercept = 0.08) +
+  geom_vline(xintercept = 0.15) +
+  labs(title="Panel B. Male") +
+  xlab("BAC") +
+  ylab("") +
+  coord_cartesian(xlim = c(0.03, 0.2), ylim = c(0.74, 0.83))
+panel_b_quadratic
+
+# panel C - age
+
+panel_c_quadratic <- ggplot(data = dwi, 
+                  aes(x = bac_min, 
+                      y = aged, 
+                      group = over_limit)) +
+  theme_clean() +
+  geom_point(data = dwi_accident_sum) +
+  geom_smooth(method = 'lm', formula= y ~ poly(x,2)) +
+  geom_vline(xintercept = 0.08) +
+  geom_vline(xintercept = 0.15) +
+  labs(title="Panel C. Age") +
+  xlab("BAC") +
+  ylab("") +
+  coord_cartesian(xlim = c(0.03, 0.2), ylim = c(33, 38))
+panel_c_quadratic
+
+# panel D - white
+
+panel_d_quadratic <- ggplot(data = dwi, 
+                  aes(x = bac_min, 
+                      y = white, 
+                      group = over_limit)) +
+  theme_clean() +
+  geom_point(data = dwi_accident_sum) +
+  geom_smooth(method = 'lm', formula= y ~ poly(x,2)) +
+  geom_vline(xintercept = 0.08) +
+  geom_vline(xintercept = 0.15) +
+  labs(title="Panel D. White") +
+  xlab("BAC") +
+  ylab("") +
+  coord_cartesian(xlim=c(0.03, 0.2), ylim=c(0.8, 0.9))
+panel_d_quadratic
+
+# Assemble panels into figure 2 replication
+figure2_quadratic <- grid.arrange(panel_a_quadratic, panel_b_quadratic, 
+                            panel_c_quadratic, panel_d_quadratic, nrow = 2)
+figure2_quadratic
