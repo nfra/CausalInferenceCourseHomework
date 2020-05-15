abdcab7c52772fd5863f7dabaaaf195731fe2304
Nathan <37715299+nfra@users.noreply.github.com>
2020-05-15 11:29:40 -0500

    update replication 3, final

--- Assignment3_DD/code/r/replication_dd_stevensonwolfers.R
+++ Assignment3_DD/code/r/replication_dd_stevensonwolfers.R
@@ -6,6 +6,7 @@ library(stargazer)
 library(knitr)
 library(lfe)
 library(gridExtra)
+library(bacondecomp)
 
 
 # read data
@@ -69,6 +70,7 @@ stargazer(homicide_fe_reg,
 homicide_fe_leadlag_reg = felm(asmrh ~ as.factor(I(year - `_nfd`)) | statename + year | 0 | statename, data = divorce)
 suicide_fe_leadlag_reg = felm(asmrs ~ as.factor(I(year - `_nfd`)) | statename + year | 0 | statename, data = divorce)
 
+summary(homicide_fe_leadlag_reg)
 
 leadlag_data <- as.data.frame(tibble(label = -20:27,
                                          mean_homicide = coef(homicide_fe_leadlag_reg)[1:48],
@@ -92,3 +94,37 @@ suicide_plot <- ggplot(data = leadlag_data, aes(x = label)) +
   ylab("Mean ASMR, suicide")
 
 grid.arrange(homicide_plot, suicide_plot, ncol=1)
+
+unique(divorce$nfd)
+
+homicide_bacon_decomp <- bacon(asmrh ~ post,
+                              data = divorce, id_var = "statename",
+                              time_var = "year")
+
+suicide_bacon_decomp <- bacon(asmrs ~ post,
+                  data = divorce, id_var = "statename",
+                  time_var = "year")
+
+homicide_bacon_decomp
+kable(suicide_bacon_decomp)
+
+suicide_bacon_decomp_plot_data = as.data.frame(suicide_bacon_decomp)
+homicide_bacon_decomp_plot_data = as.data.frame(homicide_bacon_decomp)
+
+ggplot(data = suicide_bacon_decomp_plot_data) + 
+  geom_point(aes(x = weight, y = estimate, color = type))
+
+homicide_fe_leadlag_post_reg = felm(asmrh ~ post + as.factor(I(year - `_nfd`)) | statename + year | 0 | statename, data = divorce)
+suicide_fe_leadlag_post_reg = felm(asmrs ~ post + as.factor(I(year - `_nfd`)) | statename + year | 0 | statename, data = divorce)
+
+stargazer(homicide_fe_leadlag_post_reg, 
+          suicide_fe_leadlag_post_reg,
+          header = FALSE,
+          title = "Estimates of Mean Female ASMR Due to Homicide and Suicide in Lead and Lag Years",
+          notes = "Standard errors are clustered by state.",
+          type = 'text'
+)
+
+
+summary(suicide_fe_leadlag_post_reg)
+
